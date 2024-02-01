70b769e520320ab53cd23010d81d336082510c60
Aiz <dev@aiz.moe>
2024-02-01 08:37:29 +0100

    feat: tailwind and more

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,25 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-    intergrations: [tailwind(),]
+  integrations: [
+    tailwind(),
+    icon({
+      svgoOptions: {
+        multipass: true,
+        plugins: [
+          "preset-default",
+          "removeStyleElement",
+          {
+            name: "convertColors",
+            params: {
+              currentColor: true,
+            },
+          },
+        ],
+      },
+    }),
+  ],
 });
