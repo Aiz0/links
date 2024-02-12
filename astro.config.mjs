1101bd7decf0a267d2e2805561ded0ba8f786fcb
Aiz <dev@aiz.moe>
2024-02-12 02:57:49 +0100

    feat: style, favicon, stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import favicons from "astro-favicons";
 
 // https://astro.build/config
 export default defineConfig({
@@ -21,5 +22,8 @@ export default defineConfig({
         ],
       },
     }),
+    favicons({
+      masterPicture: "./src/favicon.png"
+    })
   ],
 });
