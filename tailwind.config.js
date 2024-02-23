609ab6e12c0889a99dd78e55c4a8cf6d7b9efca5
Aiz <dev@aiz.moe>
2024-02-23 10:54:18 +0100

    feat: update style
    blurred background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,15 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {fontFamily: {
-      sans: [
-        'Roboto Condensed', ...defaultTheme.fontFamily.sans
-      ]},
+    extend: {
+      fontFamily: {
+        sans: [
+          'Roboto Condensed', ...defaultTheme.fontFamily.sans
+        ]
+      },
+      backgroundImage: {
+        'main': "url(background.png)"
+      }
     }
   },
   plugins: [],
